75681689283f438de293abbfeb108f05402ae275
pivotal-jack-moon <jomoon@pivotal.io>
2019-08-21 15:45:24 +0900

    ..

--- scripts/activate-network-service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IP_RANGE="51 55"
-for i in `seq $IP_RANGE`; do ssh cloud-user@192.168.0.$i sudo systemctl stop firewalld ; done
-for i in `seq $IP_RANGE`; do ssh cloud-user@192.168.0.$i sudo systemctl disable firewalld ; done
